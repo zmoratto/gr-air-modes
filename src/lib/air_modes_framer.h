b190fb923c57563955751162f1b1104a3edfa427
Zack Moratto <zachary.m.moratto@nasa.gov>
2011-03-13 19:09:03 -0700

    Style fix. 80 chars please
    I'm a snob for formating.

--- src/lib/air_modes_framer.h
+++ src/lib/air_modes_framer.h
@@ -1,23 +1,23 @@
 /*
-# Copyright 2010 Nick Foster
-# 
-# This file is part of gr-air-modes
-# 
-# gr-air-modes is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
-# 
-# gr-air-modes is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with gr-air-modes; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street,
-# Boston, MA 02110-1301, USA.
-# 
+  Copyright 2010 Nick Foster
+
+  This file is part of gr-air-modes
+
+  gr-air-modes is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your option)
+  any later version.
+
+  gr-air-modes is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with gr-air-modes; see the file COPYING.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street,
+  Boston, MA 02110-1301, USA.
+
 */
 
 #ifndef INCLUDED_AIR_MODES_FRAMER_H
@@ -36,19 +36,19 @@ air_modes_framer_sptr air_make_modes_framer(int channel_rate);
  */
 class air_modes_framer : public gr_sync_block
 {
-private:
-    friend air_modes_framer_sptr air_make_modes_framer(int channel_rate);
-    air_modes_framer(int channel_rate);
+ private:
+  friend air_modes_framer_sptr air_make_modes_framer(int channel_rate);
+  air_modes_framer(int channel_rate);
 
-	int d_check_width;
-	int d_chip_rate;
-	int d_samples_per_chip;
-	int d_samples_per_symbol;
+  int d_check_width;
+  int d_chip_rate;
+  int d_samples_per_chip;
+  int d_samples_per_symbol;
 
-public:
-    int work (int noutput_items,
-              gr_vector_const_void_star &input_items,
-              gr_vector_void_star &output_items);
+ public:
+  int work (int noutput_items,
+            gr_vector_const_void_star &input_items,
+            gr_vector_void_star &output_items);
 };
 
 
